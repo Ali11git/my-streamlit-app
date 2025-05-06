a65b3cea12d389e9bd70cc347d69f02fd95a9be0
Ali11git <alih20020101@gmail.com>
2025-05-06 11:36:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ import random
 import requests
 HF_TOKEN = st.secrets['HF_TOKEN']
 def generate_ai_image(prompt, width=256, height=256):
-    API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2"
+    API_URL = "https://huggingface.co/collections/stabilityai/stable-diffusion-35-671785cca799084f71fa2838"
     headers = {
         "Authorization": f"Bearer {HF_TOKEN}",
         "Content-Type": "application/json"
